ea20cab57a80448a7b0dd0f593e6b2fb515c7be0
Ussama Naal <ussama.naal@ouster.io>
2023-01-30 17:45:36 -0800

    Auto generate standalone nodes from components

--- ouster-ros/launch/sensor.composite.launch.py
+++ ouster-ros/launch/sensor.composite.launch.py
@@ -7,43 +7,44 @@ import launch
 from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
 
+
 def generate_launch_description():
     """Generate launch description with multiple components."""
     container = ComposableNodeContainer(
-            name='os_container',
-            namespace='ouster_ns',
-            package='rclcpp_components',
-            executable='component_container_mt',
-            composable_node_descriptions=[
+        name='os_container',
+        namespace='',
+        package='rclcpp_components',
+        executable='component_container_mt',
+        composable_node_descriptions=[
                 ComposableNode(
                     package='ouster_ros',
                     plugin='ouster_ros::OusterSensor',
                     name='os_sensor',
                     parameters=[
-                        {'sensor_hostname' : 'os-122151001683.local'},
-                        {'udp_dest' : ' '},
-                        {'lidar_mode' : ' '},
-                        {'timestamp_mode' : ' '},
-                        {'udp_profile_lidar' : ' '},
-                        {'metadata' : ' '},
-                        {'lidar_port' : 48853},
-                        {'imu_port' : 37028}]
+                        {'sensor_hostname': 'os-122151001683.local'},
+                        {'udp_dest': ' '},
+                        {'lidar_mode': ' '},
+                        {'timestamp_mode': ' '},
+                        {'udp_profile_lidar': ' '},
+                        {'metadata': ' '},
+                        {'lidar_port': 48853},
+                        {'imu_port': 37028}]
                 ),
-                ComposableNode(
+            ComposableNode(
                     package='ouster_ros',
                     plugin='ouster_ros::OusterCloud',
                     name='os_cloud',
                     parameters=[
-                        {'tf_prefix' : ' '},
-                        {'timestamp_mode' : ' '}]
+                        {'tf_prefix': ' '},
+                        {'timestamp_mode': ' '}]
                 ),
-                ComposableNode(
+            ComposableNode(
                     package='ouster_ros',
                     plugin='ouster_ros::OusterImage',
                     name='os_image'
                 )
-            ],
-            output='screen',
+        ],
+        output='screen',
     )
 
     return launch.LaunchDescription([container])
--- ouster-ros/launch/sensor.separate.launch.py
+++ ouster-ros/launch/sensor.separate.launch.py
@@ -4,74 +4,49 @@
 """Launch a sensor node along with..."""
 
 import launch
-from launch_ros.actions import ComposableNodeContainer
-from launch_ros.descriptions import ComposableNode
+from launch_ros.actions import Node
 from launch.actions import TimerAction
 
+
 def generate_launch_description():
-    """Generate launch description with multiple components."""
-    os_sensor_container = ComposableNodeContainer(
-            name='os_sensor_container',
-            namespace='ouster_ns',
-            package='rclcpp_components',
-            executable='component_container',
-            composable_node_descriptions=[
-                ComposableNode(
-                    package='ouster_ros',
-                    plugin='ouster_ros::OusterSensor',
-                    name='os_sensor',
-                    parameters=[
-                        {'sensor_hostname' : 'os-122151001683.local'},
-                        {'udp_dest' : ' '},
-                        {'lidar_mode' : ' '},
-                        {'timestamp_mode' : ' '},
-                        {'udp_profile_lidar' : ' '},
-                        {'metadata' : ' '},
-                        {'lidar_port' : 48853},
-                        {'imu_port' : 37028}]
-                )
-            ],
-            output='screen',
+    """Generate launch description for ouster_ros."""
+    os_sensor = Node(
+        name='os_sensor',
+        namespace='',
+        package='ouster_ros',
+        executable='os_sensor',
+        parameters=[
+                {'sensor_hostname': 'os-122151001683.local'},
+                {'udp_dest': ' '},
+                {'lidar_mode': ' '},
+                {'timestamp_mode': ' '},
+                {'udp_profile_lidar': ' '},
+                {'metadata': ' '},
+                {'lidar_port': 48853},
+                {'imu_port': 37028}],
+        output='screen',
     )
 
-    os_cloud_container = ComposableNodeContainer(
-            name='os_cloud_container',
-            namespace='ouster_ns',
-            package='rclcpp_components',
-            executable='component_container',
-            composable_node_descriptions=[
-                ComposableNode(
-                    package='ouster_ros',
-                    plugin='ouster_ros::OusterCloud',
-                    name='os_cloud',
-                    parameters=[
-                        {'tf_prefix' : ' '},
-                        {'timestamp_mode' : ' '}]
-                )
-            ],
-            output='screen',
+    os_cloud = Node(
+        name='os_cloud',
+        namespace='',
+        package='ouster_ros',
+        executable='os_cloud',
+        parameters=[
+            {'tf_prefix': ' '},
+            {'timestamp_mode': ' '}],
+        output='screen',
     )
 
-    os_cloud_container_delayed = TimerAction(period=10.0, actions=[os_cloud_container])
-
-    os_image_container = ComposableNodeContainer(
-            name='os_image_container',
-            namespace='ouster_ns',
-            package='rclcpp_components',
-            executable='component_container',
-            composable_node_descriptions=[
-                ComposableNode(
-                    package='ouster_ros',
-                    plugin='ouster_ros::OusterImage',
-                    name='os_image'
-                )
-            ],
-            output='screen',
+    os_image = Node(
+        name='os_image',
+        namespace='',
+        package='ouster_ros',
+        executable='os_image',
+        output='screen',
     )
-
-    os_image_container_delayed = TimerAction(period=10.0, actions=[os_image_container])
 
     return launch.LaunchDescription([
-        os_sensor_container,
-        os_cloud_container_delayed,
-        os_image_container_delayed])
+        os_sensor,
+        TimerAction(period=3.0, actions=[os_cloud]),
+        TimerAction(period=4.0, actions=[os_image])])
