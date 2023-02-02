a9292746e56ce38092347220bab036a6589151a1
Ussama Naal <ussama.naal@ouster.io>
2023-02-02 15:53:40 -0800

    Add a note about missing support of parameterize ros namespace when using launch.py

--- ouster-ros/launch/sensor.composite.launch.py
+++ ouster-ros/launch/sensor.composite.launch.py
@@ -23,39 +23,45 @@ def generate_launch_description():
     ouster_ros_pkg_dir = get_package_share_directory('ouster_ros')
     params = Path(ouster_ros_pkg_dir) / 'config' / 'parameters.yaml'
 
+    # TODO: changing the default namespace is currently not supported when using the launch.py format'
+    ouster_ns = LaunchConfiguration('ouster_ns')
+    ouster_ns_launch_arg = DeclareLaunchArgument(
+        'ouster_ns', default_value='ouster')
+
+    viz_launch_config = LaunchConfiguration('viz')
+    viz_launch_arg = DeclareLaunchArgument('viz', default_value='True')
+
     os_container = ComposableNodeContainer(
         name='os_container',
-        namespace='ouster',
+        namespace=ouster_ns,
         package='rclcpp_components',
         executable='component_container_mt',
         composable_node_descriptions=[
-                ComposableNode(
-                    package='ouster_ros',
-                    plugin='ouster_ros::OusterSensor',
-                    name='os_sensor',
-                    namespace='ouster',
-                    parameters=[params]
-                ),
             ComposableNode(
-                    package='ouster_ros',
-                    plugin='ouster_ros::OusterCloud',
-                    name='os_cloud',
-                    namespace='ouster',
-                    parameters=[params]
+                package='ouster_ros',
+                plugin='ouster_ros::OusterSensor',
+                name='os_sensor',
+                namespace=ouster_ns,
+                parameters=[params]
             ),
             ComposableNode(
-                    package='ouster_ros',
-                    plugin='ouster_ros::OusterImage',
-                    name='os_image',
-                    namespace='ouster',
-                    parameters=[params]
+                package='ouster_ros',
+                plugin='ouster_ros::OusterCloud',
+                name='os_cloud',
+                namespace=ouster_ns,
+                parameters=[params]
+            ),
+            ComposableNode(
+                package='ouster_ros',
+                plugin='ouster_ros::OusterImage',
+                name='os_image',
+                namespace=ouster_ns,
+                parameters=[params]
             )
         ],
         output='screen',
     )
 
-    viz_launch_config = LaunchConfiguration('viz')
-    viz_launch_arg = DeclareLaunchArgument('viz', default_value='True')
     viz_launch_file_path = \
         Path(ouster_ros_pkg_dir) / 'launch' / 'sensor.rviz.launch.py'
     rviz_launch = IncludeLaunchDescription(
@@ -66,18 +72,18 @@ def generate_launch_description():
     # NOTE: this is rather a workaround to let rviz2 get going and not complain while waiting
     #     for the actual sensor frames to be published that is when when using the same launch file
     #     to run rviz2
-    sensor_imu_tf = Node(package = "tf2_ros", 
-                       executable = "static_transform_publisher",
-                       name = "stp_sensor_imu",
-                       condition=IfCondition(viz_launch_config),
-                       arguments = ["0", "0", "0", "0", "0", "0", "os_sensor", "os_imu"])
-    sensor_lidar_tf = Node(package = "tf2_ros", 
-                       executable = "static_transform_publisher",
-                       name = "stp_sensor_lidar",
-                       condition=IfCondition(viz_launch_config),
-                       arguments = ["0", "0", "0", "0", "0", "0", "os_sensor", "os_lidar"])
-
+    sensor_imu_tf = Node(package="tf2_ros",
+                         executable="static_transform_publisher",
+                         name="stp_sensor_imu",
+                         condition=IfCondition(viz_launch_config),
+                         arguments=["0", "0", "0", "0", "0", "0", "os_sensor", "os_imu"])
+    sensor_ldr_tf = Node(package="tf2_ros",
+                         executable="static_transform_publisher",
+                         name="stp_sensor_lidar",
+                         condition=IfCondition(viz_launch_config),
+                         arguments=["0", "0", "0", "0", "0", "0", "os_sensor", "os_lidar"])
 
     return launch.LaunchDescription([
+        ouster_ns_launch_arg,
         os_container,
-        viz_launch_arg, sensor_imu_tf, sensor_lidar_tf, rviz_launch])
+        viz_launch_arg, sensor_imu_tf, sensor_ldr_tf, rviz_launch])
--- ouster-ros/launch/sensor.independent.launch.py
+++ ouster-ros/launch/sensor.independent.launch.py
@@ -21,11 +21,19 @@ def generate_launch_description():
     ouster_ros_pkg_dir = get_package_share_directory('ouster_ros')
     params = Path(ouster_ros_pkg_dir) / 'config' / 'parameters.yaml'
 
+    # TODO: changing the default namespace is currently not supported when using the launch.py format'
+    ouster_ns = LaunchConfiguration('ouster_ns')
+    ouster_ns_launch_arg = DeclareLaunchArgument(
+        'ouster_ns', default_value='ouster')
+
+    viz_launch_config = LaunchConfiguration('viz')
+    viz_launch_arg = DeclareLaunchArgument('viz', default_value='True')
+
     os_sensor = Node(
         package='ouster_ros',
         executable='os_sensor',
         name='os_sensor',
-        namespace='ouster',
+        namespace=ouster_ns,
         parameters=[params],
         output='screen',
     )
@@ -34,7 +42,7 @@ def generate_launch_description():
         package='ouster_ros',
         executable='os_cloud',
         name='os_cloud',
-        namespace='ouster',
+        namespace=ouster_ns,
         parameters=[params],
         output='screen',
     )
@@ -43,13 +51,11 @@ def generate_launch_description():
         package='ouster_ros',
         executable='os_image',
         name='os_image',
-        namespace='ouster',
+        namespace=ouster_ns,
         parameters=[params],
         output='screen',
     )
 
-    viz_launch_config = LaunchConfiguration('viz')
-    viz_launch_arg = DeclareLaunchArgument('viz', default_value='True')
     viz_launch_file_path = \
         Path(ouster_ros_pkg_dir) / 'launch' / 'sensor.rviz.launch.py'
     rviz_launch = IncludeLaunchDescription(
@@ -60,17 +66,20 @@ def generate_launch_description():
     # NOTE: this is rather a workaround to let rviz2 get going and not complain while waiting
     #     for the actual sensor frames to be published that is when when using the same launch file
     #     to run rviz2
-    sensor_imu_tf = Node(package = "tf2_ros", 
-                       executable = "static_transform_publisher",
-                       name = "stp_sensor_imu",
-                       condition=IfCondition(viz_launch_config),
-                       arguments = ["0", "0", "0", "0", "0", "0", "os_sensor", "os_imu"])
-    sensor_lidar_tf = Node(package = "tf2_ros", 
-                       executable = "static_transform_publisher",
-                       name = "stp_sensor_lidar",
-                       condition=IfCondition(viz_launch_config),
-                       arguments = ["0", "0", "0", "0", "0", "0", "os_sensor", "os_lidar"])
+    sensor_imu_tf = Node(package="tf2_ros",
+                         executable="static_transform_publisher",
+                         name="stp_sensor_imu",
+                         namespace=ouster_ns,
+                         condition=IfCondition(viz_launch_config),
+                         arguments=["0", "0", "0", "0", "0", "0", "os_sensor", "os_imu"])
+    sensor_ldr_tf = Node(package="tf2_ros",
+                         executable="static_transform_publisher",
+                         name="stp_sensor_lidar",
+                         namespace=ouster_ns,
+                         condition=IfCondition(viz_launch_config),
+                         arguments=["0", "0", "0", "0", "0", "0", "os_sensor", "os_lidar"])
 
     return launch.LaunchDescription([
+        ouster_ns_launch_arg,
         os_sensor, os_cloud, os_image,
-        viz_launch_arg, sensor_imu_tf, sensor_lidar_tf, rviz_launch])
+        viz_launch_arg, sensor_imu_tf, sensor_ldr_tf, rviz_launch])
