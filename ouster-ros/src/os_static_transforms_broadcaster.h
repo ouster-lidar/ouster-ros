9cbb840fe9e343e2122d1bc77007aa8d7a5baad7
Ussama Naal <606033+Samahu@users.noreply.github.com>
2023-07-14 09:45:10 -0700

    SW-5167: follow ups from SW-5167-fix-black-columns-when-recording-under-ros-foxy (#163)
    * Initialize point_cloud_frame with an empty string + and expose point_cloud_frame through launch xml files +
    Remove unused/duplicate param definitions + Update descriptions
    * Correct the name of the node to be activated for merged node

--- ouster-ros/src/os_static_transforms_broadcaster.h
+++ ouster-ros/src/os_static_transforms_broadcaster.h
@@ -22,7 +22,7 @@ class OusterStaticTransformsBroadcaster {
         node->declare_parameter("sensor_frame", "os_sensor");
         node->declare_parameter("lidar_frame", "os_lidar");
         node->declare_parameter("imu_frame", "os_imu");
-        node->declare_parameter("point_cloud_frame", "os_lidar");
+        node->declare_parameter("point_cloud_frame", "");
     }
 
     void parse_parameters() {
