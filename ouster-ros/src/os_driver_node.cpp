0b53b1d5b046c85fdc86c3be79c077976bc7a302
Ussama Naal <ussama.naal@ouster.io>
2023-11-14 15:58:55 -0800

    Port changes from ros2 main branch to the foxy branch

--- ouster-ros/src/os_driver_node.cpp
+++ ouster-ros/src/os_driver_node.cpp
@@ -21,6 +21,7 @@
 #include "point_cloud_processor.h"
 #include "laser_scan_processor.h"
 #include "image_processor.h"
+#include "point_cloud_processor_factory.h"
 
 namespace ouster_ros {
 
@@ -36,6 +37,7 @@ class OusterDriver : public OusterSensor {
         declare_parameter("proc_mask", "IMU|IMG|PCL|SCAN");
         declare_parameter("scan_ring", 0);
         declare_parameter("ptp_utc_tai_offset", -37.0);
+        declare_parameter("point_type", "original");
     }
 
     LifecycleNodeInterface::CallbackReturn on_activate(
@@ -58,7 +60,7 @@ class OusterDriver : public OusterSensor {
 
     virtual void create_publishers() override {
         auto proc_mask = get_parameter("proc_mask").as_string();
-        auto tokens = parse_tokens(proc_mask, '|');
+        auto tokens = impl::parse_tokens(proc_mask, '|');
 
         bool use_system_default_qos =
             get_parameter("use_system_default_qos").as_bool();
@@ -71,7 +73,7 @@ class OusterDriver : public OusterSensor {
         auto ptp_utc_tai_offset =
             get_parameter("ptp_utc_tai_offset").as_double();
 
-        if (check_token(tokens, "IMU")) {
+        if (impl::check_token(tokens, "IMU")) {
             imu_pub =
                 create_publisher<sensor_msgs::msg::Imu>("imu", selected_qos);
             imu_packet_handler = ImuPacketHandler::create_handler(
@@ -82,23 +84,32 @@ class OusterDriver : public OusterSensor {
         int num_returns = get_n_returns(info);
 
         std::vector<LidarScanProcessor> processors;
-        if (check_token(tokens, "PCL")) {
+        if (impl::check_token(tokens, "PCL")) {
             lidar_pubs.resize(num_returns);
             for (int i = 0; i < num_returns; ++i) {
                 lidar_pubs[i] = create_publisher<sensor_msgs::msg::PointCloud2>(
                     topic_for_return("points", i), selected_qos);
             }
 
-            processors.push_back(PointCloudProcessor::create(
-                info, tf_bcast.point_cloud_frame_id(),
-                tf_bcast.apply_lidar_to_sensor_transform(),
-                [this](PointCloudProcessor::OutputType msgs) {
-                    for (size_t i = 0; i < msgs.size(); ++i)
-                        lidar_pubs[i]->publish(*msgs[i]);
-                }));
+            auto point_type = get_parameter("point_type").as_string();
+            processors.push_back(
+                PointCloudProcessorFactory::create_point_cloud_processor(point_type, info,
+                    tf_bcast.point_cloud_frame_id(), tf_bcast.apply_lidar_to_sensor_transform(),
+                    [this](PointCloudProcessor_OutputType msgs) {
+                        for (size_t i = 0; i < msgs.size(); ++i) lidar_pubs[i]->publish(*msgs[i]);
+                    }
+                )
+            );
+
+            // warn about profile incompatibility
+            if (PointCloudProcessorFactory::point_type_requires_intensity(point_type) &&
+                info.format.udp_profile_lidar == UDPProfileLidar::PROFILE_RNG15_RFL8_NIR8) {
+                RCLCPP_WARN_STREAM(get_logger(),
+                    "selected point type '" << point_type << "' is not compatible with the current udp profile: RNG15_RFL8_NIR8");
+            }
         }
 
-        if (check_token(tokens, "SCAN")) {
+        if (impl::check_token(tokens, "SCAN")) {
             scan_pubs.resize(num_returns);
             for (int i = 0; i < num_returns; ++i) {
                 scan_pubs[i] = create_publisher<sensor_msgs::msg::LaserScan>(
@@ -128,7 +139,7 @@ class OusterDriver : public OusterSensor {
                 }));
         }
 
-        if (check_token(tokens, "IMG")) {
+        if (impl::check_token(tokens, "IMG")) {
             const std::map<sensor::ChanField, std::string>
                 channel_field_topic_map_1{
                     {sensor::ChanField::RANGE, "range_image"},
@@ -163,8 +174,8 @@ class OusterDriver : public OusterSensor {
                 }));
         }
 
-        if (check_token(tokens, "PCL") || check_token(tokens, "SCAN") ||
-            check_token(tokens, "IMG"))
+        if (impl::check_token(tokens, "PCL") || impl::check_token(tokens, "SCAN") ||
+            impl::check_token(tokens, "IMG"))
             lidar_packet_handler = LidarPacketHandler::create_handler(
                 info, processors, timestamp_mode,
                 static_cast<int64_t>(ptp_utc_tai_offset * 1e+9));
---
+++ ouster-ros/src/point_cloud_processor_factory.h
@@ -0,0 +1,154 @@
+#pragma once
+
+#include "point_cloud_processor.h"
+
+namespace ouster_ros {
+
+namespace sensor = ouster::sensor;
+using sensor::UDPProfileLidar;
+
+class PointCloudProcessorFactory {
+    template <typename PointT>
+    static typename PointCloudProcessor<PointT>::ScanToCloudFn
+    make_scan_to_cloud_fn(const sensor::sensor_info& info) {
+        switch (info.format.udp_profile_lidar) {
+            case UDPProfileLidar::PROFILE_LIDAR_LEGACY:
+                return [](ouster_ros::Cloud<PointT>& cloud,
+                          const ouster::PointsF& points, uint64_t scan_ts,
+                          const ouster::LidarScan& ls,
+                          const std::vector<int>& pixel_shift_by_row,
+                          int return_index) {
+                    unused_variable(return_index);
+                    Point_LEGACY staging_pt;
+                    scan_to_cloud_f_destaggered<Profile_LEGACY.size(),
+                                                Profile_LEGACY>(
+                        cloud, staging_pt, points, scan_ts, ls,
+                        pixel_shift_by_row);
+                };
+
+            case UDPProfileLidar::PROFILE_RNG19_RFL8_SIG16_NIR16_DUAL:
+                return [](ouster_ros::Cloud<PointT>& cloud,
+                          const ouster::PointsF& points, uint64_t scan_ts,
+                          const ouster::LidarScan& ls,
+                          const std::vector<int>& pixel_shift_by_row,
+                          int return_index) {
+                    Point_RNG19_RFL8_SIG16_NIR16_DUAL staging_pt;
+                    if (return_index == 0) {
+                        scan_to_cloud_f_destaggered<
+                            Profile_RNG19_RFL8_SIG16_NIR16_DUAL.size(),
+                            Profile_RNG19_RFL8_SIG16_NIR16_DUAL>(
+                            cloud, staging_pt, points, scan_ts, ls,
+                            pixel_shift_by_row);
+                    } else {
+                        scan_to_cloud_f_destaggered<
+                            Profile_RNG19_RFL8_SIG16_NIR16_DUAL_2ND_RETURN
+                                .size(),
+                            Profile_RNG19_RFL8_SIG16_NIR16_DUAL_2ND_RETURN>(
+                            cloud, staging_pt, points, scan_ts, ls,
+                            pixel_shift_by_row);
+                    }
+                };
+
+            case UDPProfileLidar::PROFILE_RNG19_RFL8_SIG16_NIR16:
+                return [](ouster_ros::Cloud<PointT>& cloud,
+                          const ouster::PointsF& points, uint64_t scan_ts,
+                          const ouster::LidarScan& ls,
+                          const std::vector<int>& pixel_shift_by_row,
+                          int return_index) {
+                    unused_variable(return_index);
+                    Point_RNG19_RFL8_SIG16_NIR16 staging_pt;
+                    scan_to_cloud_f_destaggered<
+                        Profile_RNG19_RFL8_SIG16_NIR16.size(),
+                        Profile_RNG19_RFL8_SIG16_NIR16>(cloud, staging_pt,
+                                                        points, scan_ts, ls,
+                                                        pixel_shift_by_row);
+                };
+
+            case UDPProfileLidar::PROFILE_RNG15_RFL8_NIR8:
+                return [](ouster_ros::Cloud<PointT>& cloud,
+                          const ouster::PointsF& points, uint64_t scan_ts,
+                          const ouster::LidarScan& ls,
+                          const std::vector<int>& pixel_shift_by_row,
+                          int return_index) {
+                    unused_variable(return_index);
+                    Point_RNG15_RFL8_NIR8 staging_pt;
+                    scan_to_cloud_f_destaggered<Profile_RNG15_RFL8_NIR8.size(),
+                                                Profile_RNG15_RFL8_NIR8>(
+                        cloud, staging_pt, points, scan_ts, ls,
+                        pixel_shift_by_row);
+                };
+
+            default:
+                throw std::runtime_error("unsupported udp_profile_lidar");
+        }
+    }
+
+    template <typename PointT>
+    static LidarScanProcessor make_point_cloud_procssor(
+        const sensor::sensor_info& info, const std::string& frame,
+        bool apply_lidar_to_sensor_transform,
+        PointCloudProcessor_PostProcessingFn post_processing_fn) {
+        auto scan_to_cloud_fn = make_scan_to_cloud_fn<PointT>(info);
+        return PointCloudProcessor<PointT>::create(
+            info, frame, apply_lidar_to_sensor_transform, scan_to_cloud_fn,
+            post_processing_fn);
+    }
+
+   public:
+    static bool point_type_requires_intensity(const std::string& point_type) {
+        return point_type == "xyzi" || point_type == "xyzir" ||
+               point_type == "original";
+    }
+
+    static LidarScanProcessor create_point_cloud_processor(
+        const std::string& point_type, const sensor::sensor_info& info,
+        const std::string& frame, bool apply_lidar_to_sensor_transform,
+        PointCloudProcessor_PostProcessingFn post_processing_fn) {
+        if (point_type == "native") {
+            switch (info.format.udp_profile_lidar) {
+                case UDPProfileLidar::PROFILE_LIDAR_LEGACY:
+                    return make_point_cloud_procssor<Point_LEGACY>(
+                        info, frame, apply_lidar_to_sensor_transform,
+                        post_processing_fn);
+                case UDPProfileLidar::PROFILE_RNG19_RFL8_SIG16_NIR16_DUAL:
+                    return make_point_cloud_procssor<
+                        Point_RNG19_RFL8_SIG16_NIR16_DUAL>(
+                        info, frame, apply_lidar_to_sensor_transform,
+                        post_processing_fn);
+                case UDPProfileLidar::PROFILE_RNG19_RFL8_SIG16_NIR16:
+                    return make_point_cloud_procssor<
+                        Point_RNG19_RFL8_SIG16_NIR16>(
+                        info, frame, apply_lidar_to_sensor_transform,
+                        post_processing_fn);
+                case UDPProfileLidar::PROFILE_RNG15_RFL8_NIR8:
+                    return make_point_cloud_procssor<Point_RNG15_RFL8_NIR8>(
+                        info, frame, apply_lidar_to_sensor_transform,
+                        post_processing_fn);
+                default:
+                    // TODO: implement fallback?
+                    throw std::runtime_error("unsupported udp_profile_lidar");
+            }
+        } else if (point_type == "xyz") {
+            return make_point_cloud_procssor<pcl::PointXYZ>(
+                info, frame, apply_lidar_to_sensor_transform,
+                post_processing_fn);
+        } else if (point_type == "xyzi") {
+            return make_point_cloud_procssor<pcl::PointXYZI>(
+                info, frame, apply_lidar_to_sensor_transform,
+                post_processing_fn);
+        } else if (point_type == "xyzir") {
+            return make_point_cloud_procssor<PointXYZIR>(
+                info, frame, apply_lidar_to_sensor_transform,
+                post_processing_fn);
+        } else if (point_type == "original") {
+            return make_point_cloud_procssor<ouster_ros::Point>(
+                info, frame, apply_lidar_to_sensor_transform,
+                post_processing_fn);
+        }
+
+        throw std::runtime_error(
+            "Un-supported point type used: " + point_type + "!");
+    }
+};
+
+}  // namespace ouster_ros
