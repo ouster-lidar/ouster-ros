3a396ad4832c6ac50187bc3930670b9427702494
Ussama Naal <ussama.naal@ouster.io>
2023-11-23 12:02:52 -0800

    address an issue where LaserScan appeared different on FW prior to 2.4

--- ouster-ros/src/laser_scan_processor.h
+++ ouster-ros/src/laser_scan_processor.h
@@ -8,7 +8,7 @@
 
 #pragma once
 
-// prevent clang-format from altering the location of "ouster_ros/ros.h", the
+// prevent clang-format from altering the location of "ouster_ros/os_ros.h", the
 // header file needs to be the first include due to PCL_NO_PRECOMPILE flag
 // clang-format off
 #include "ouster_ros/os_ros.h"
@@ -32,16 +32,25 @@ class LaserScanProcessor {
           pixel_shift_by_row(info.format.pixel_shift_by_row),
           scan_msgs(get_n_returns(info)),
           post_processing_fn(func) {
-            for (size_t i = 0; i < scan_msgs.size(); ++i)
-                scan_msgs[i] = std::make_shared<sensor_msgs::msg::LaserScan>();
-          }
+        for (size_t i = 0; i < scan_msgs.size(); ++i)
+            scan_msgs[i] = std::make_shared<sensor_msgs::msg::LaserScan>();
+
+        const auto fw = impl::parse_version(info.fw_rev);
+        if (fw.major == 2 && fw.minor < 4) {
+            std::transform(pixel_shift_by_row.begin(),
+                           pixel_shift_by_row.end(),
+                           pixel_shift_by_row.begin(),
+                           [](auto c) { return c - 31; });
+        }
+    }
 
    private:
     void process(const ouster::LidarScan& lidar_scan, uint64_t,
                  const rclcpp::Time& msg_ts) {
-        for (int i = 0; i < static_cast<int>(scan_msgs.size()); ++i) {
-            *scan_msgs[i] = lidar_scan_to_laser_scan_msg(
-                lidar_scan, msg_ts, frame, ld_mode, ring_, pixel_shift_by_row, i);
+        for (size_t i = 0; i < scan_msgs.size(); ++i) {
+            *scan_msgs[i] =
+                lidar_scan_to_laser_scan_msg(lidar_scan, msg_ts, frame, ld_mode,
+                                             ring_, pixel_shift_by_row, i);
         }
 
         if (post_processing_fn) post_processing_fn(scan_msgs);
