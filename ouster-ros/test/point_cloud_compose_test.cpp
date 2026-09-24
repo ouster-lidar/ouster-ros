5416645a990cd489f5da86c8753222fa79ff8736
Ussama Naal <ussama.naal@ouster.io>
2026-09-24 11:08:19 -0700

    Fix the bug introduced by avoiding the modulo in  scan_to_cloud

--- ouster-ros/test/point_cloud_compose_test.cpp
+++ ouster-ros/test/point_cloud_compose_test.cpp
@@ -11,6 +11,7 @@
 #include "ouster_ros/os_point.h"
 #include "../src/point_meta_helpers.h"
 #include "../src/point_cloud_compose.h"
+#include "../src/point_cloud_processor.h"
 
 class PointCloudComposeTest : public ::testing::Test {
    protected:
@@ -71,4 +72,74 @@ TEST_F(PointCloudComposeTest, MapLidarScanFields) {
         EXPECT_EQ(point::get<7>(pt), reflect(0, src_idx));
         EXPECT_EQ(point::get<8>(pt), near_ir(0, src_idx));
     }
+}
+
+TEST_F(PointCloudComposeTest, ScanToCloudDestaggerHandlesNegativePixelShift) {
+    const auto WIDTH = 8;
+    const auto HEIGHT = 6;
+    const auto SAMPLES = WIDTH * HEIGHT;
+
+    LidarScan ls(WIDTH, HEIGHT, UDPProfileLidar::RNG19_RFL8_SIG16_NIR16);
+    auto range = ls.field<uint32_t>(ChanField::RANGE);
+    for (auto i = 0; i < SAMPLES; ++i) range.data()[i] = i + 1;
+
+    PointCloudXYZf points = PointCloudXYZf::Zero(SAMPLES, 3);
+
+    // mix of positive, zero, negative and boundary shifts within [-w, w)
+    const std::vector<int> pixel_shift_by_row{3, 0, -3, WIDTH - 1, -WIDTH, -1};
+
+    ouster_ros::Cloud<Point_RNG19_RFL8_SIG16_NIR16> cloud{WIDTH, HEIGHT};
+    Point_RNG19_RFL8_SIG16_NIR16 staging_pt;
+    scan_to_cloud_f<Profile_RNG19_RFL8_SIG16_NIR16.size(),
+                    Profile_RNG19_RFL8_SIG16_NIR16>(
+        cloud, staging_pt, points, 0, ls, pixel_shift_by_row, true, true);
+
+    for (auto u = 0; u < HEIGHT; ++u) {
+        const auto s = ((pixel_shift_by_row[u] % WIDTH) + WIDTH) % WIDTH;
+        for (auto v = 0; v < WIDTH; ++v) {
+            const auto expected_v = (v + WIDTH - s) % WIDTH;
+            EXPECT_EQ(cloud.points[u * WIDTH + v].range,
+                      range(u, expected_v))
+                << "u=" << u << " v=" << v;
+        }
+    }
+}
+
+namespace {
+PointCloudProcessor<Point_RNG19_RFL8_SIG16_NIR16> make_processor(
+    const SensorInfo& info) {
+    return PointCloudProcessor<Point_RNG19_RFL8_SIG16_NIR16>(
+        info, "os_lidar", false, 0, 1000000, 1, "",
+        [](auto&, const auto&, auto, const auto&, const auto&, auto) {},
+        [](auto) {});
+}
+}  // namespace
+
+TEST_F(PointCloudComposeTest, PointCloudProcessorAcceptsPixelShiftInRange) {
+    auto info = default_sensor_info(LidarMode::_512x10);
+    const int w = static_cast<int>(info.format.columns_per_frame);
+    auto& shifts = info.format.pixel_shift_by_row;
+    shifts.assign(shifts.size(), 0);
+    shifts.front() = -w;
+    shifts.back() = w - 1;
+    EXPECT_NO_THROW(make_processor(info));
+}
+
+TEST_F(PointCloudComposeTest, PointCloudProcessorRejectsPixelShiftOutOfRange) {
+    auto info = default_sensor_info(LidarMode::_512x10);
+    const int w = static_cast<int>(info.format.columns_per_frame);
+    auto& shifts = info.format.pixel_shift_by_row;
+    shifts.assign(shifts.size(), 0);
+
+    shifts[1] = w;
+    EXPECT_THROW(make_processor(info), std::invalid_argument);
+
+    shifts[1] = -w - 1;
+    EXPECT_THROW(make_processor(info), std::invalid_argument);
+}
+
+TEST_F(PointCloudComposeTest, PointCloudProcessorRejectsPixelShiftSizeMismatch) {
+    auto info = default_sensor_info(LidarMode::_512x10);
+    info.format.pixel_shift_by_row.pop_back();
+    EXPECT_THROW(make_processor(info), std::invalid_argument);
 }
